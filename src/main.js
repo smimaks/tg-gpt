169f19c0a7ceaeedac72fdb0a62b3e224a2650fd
Susharnik Maksim <susharnikmi@lad24.ru>
2023-05-17 11:23:08 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import { init } from './init.js';
 const minute = 60000;
 const hour = minute * 60;
 const interval = 4 * hour;
-setInterval(init, minute / 6);
-console.log(new Date().toISOString(), ' Приложение запущено');
+setInterval(init, interval);
+console.log(new Date().toISOString(), 'Приложение запущено');
